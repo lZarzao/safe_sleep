cac3d95392e249796f7085693d8bbaf6e30eaaf0
luis.zarza <luis.zarza@edge.com.py>
2023-12-13 02:30:17 -0300

    Adding socket.io in the server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Server as SocketIO } from 'socket.io';
 
 import { signupRouter } from './routes/signup.js';
 import { loginRouter } from './routes/login.js';
@@ -13,6 +15,13 @@ import mongoose from 'mongoose';
 import authenticate from './auth/authenticate.js';
 
 const app = express();
+const server = http.createServer(app);
+const io = new SocketIO(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 
 dotenv.config();
 
@@ -37,6 +46,23 @@ app.use('/api/signout', signoutRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Socket.IO event handling
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+  });
+
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
 });
 
 app.listen(port, () => {
