a7a05a01f7cba5c280815ce5cd5dc9f77344611b
luis.zarza <luis.zarza@edge.com.py>
2023-12-14 02:16:35 -0300

    Fixing dashboar

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,74 +1,16 @@
-import { useEffect, useState, useContext } from 'react';
+import { useContext } from 'react';
 import { useAuth } from '../auth/AuthProvider.js';
 import { SocketContext } from '../context/SocketContext';
 import PortalLayout from '../layout/PortalLayout.jsx';
-import API_URL from '../constants/constants';
 
 const Dashboard = () => {
-  const [todo, setTodo] = useState([]);
-  const [title, setTitle] = useState('');
   const auth = useAuth();
   const { me } = useContext(SocketContext);
-
-  useEffect(() => {
-    loadTodo();
-  }, []);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    createTodo();
-  };
-
-  const createTodo = async () => {
-    try {
-      const response = await fetch(`${API_URL}/todo`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${auth.getAccessToken()}`,
-        },
-        body: JSON.stringify({
-          title,
-        }),
-      });
-
-      if (response.ok) {
-        const json = await response.json();
-        setTodo([json, ...todo]);
-      } else {
-        // Mostrar error de conexión
-      }
-    } catch (error) {}
-  };
-
-  const loadTodo = async () => {
-    try {
-      const response = await fetch(`${API_URL}/todo`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${auth.getAccessToken()}`,
-        },
-      });
-
-      if (response.ok) {
-        const json = await response.json();
-        setTodo(json);
-      } else {
-        // Mostrar error de conexión
-      }
-    } catch (error) {}
-  };
 
   return (
     <PortalLayout>
       <h1>Dashboard de {auth.getUser()?.name || ''}</h1>
       <p>Mi Socket ID: {me}</p>
-      <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='Nuevo todo...' onChange={(e) => setTitle(e.target.value)} value={title} />
-      </form>
-      {todo.map((td) => (
-        <div key='td._id'>{td.title}</div>
-      ))}
     </PortalLayout>
   );
 };
